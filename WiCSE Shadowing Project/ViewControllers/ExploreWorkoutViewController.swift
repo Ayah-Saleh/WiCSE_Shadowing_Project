8801a8cc48fc3fa794015e4706f3be567027486c
Monika Patel <patelmonika006@gmail.com>
2023-04-20 02:25:51 +0530

    Fixed Drop Issue Task and Load data in Explore Screen

--- WiCSE Shadowing Project/ViewControllers/ExploreWorkoutViewController.swift
+++ WiCSE Shadowing Project/ViewControllers/ExploreWorkoutViewController.swift
@@ -12,8 +12,10 @@ import FirebaseFirestore
 class ExploreWorkoutViewController: UIViewController {
     
     let db = Firestore.firestore()
+    var workoutsArr: [AnyObject] = []
     
     @IBOutlet weak var workoutStackView: UIStackView!
+    @IBOutlet weak var aTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,66 +24,52 @@ class ExploreWorkoutViewController: UIViewController {
     
     func fetchWorkouts() {
         db.collection("Workouts").getDocuments { [weak self] (querySnapshot, error) in
-            guard let documents = querySnapshot?.documents, error == nil else {
+            guard let documents = querySnapshot?.documents, error == nil  else {
                 print("Error fetching documents: \(error!)")
                 return
             }
-            
-            for document in documents {
-                let workout = document.data()
-                let name = workout["name"] as? String ?? ""
-                let muscleGroup = workout["muscle_group"] as? String ?? ""
-                let workoutType = workout["type"] as? String ?? ""
-                let skillLevel = workout["skill_level"] as? String ?? ""
-                let goal = workout["goal"] as? String ?? ""
-                let description = workout["workout_description"] as? String ?? ""
-                
-                let workoutView = UIView()
-                workoutView.backgroundColor = .green
-                workoutView.layer.cornerRadius = 10
-                workoutView.layer.masksToBounds = true
-                
-                let workoutName = UILabel()
-                workoutName.text = name
-                workoutName.font = UIFont.boldSystemFont(ofSize: 18)
-                workoutName.numberOfLines = 0
-                
-                let workoutDetails = UILabel()
-                workoutDetails.text = "\(muscleGroup) | \(workoutType) | \(skillLevel) | \(goal)"
-                workoutDetails.font = UIFont.systemFont(ofSize: 14)
-                workoutDetails.textColor = .white
-                workoutDetails.numberOfLines = 0
-                
-                let workoutDescription = UILabel()
-                workoutDescription.text = description
-                workoutDescription.font = UIFont.systemFont(ofSize: 14)
-                workoutDescription.numberOfLines = 0
-                
-                workoutView.addSubview(workoutName)
-                workoutName.translatesAutoresizingMaskIntoConstraints = false
-                workoutName.topAnchor.constraint(equalTo: workoutView.topAnchor, constant: 10).isActive = true
-                workoutName.leadingAnchor.constraint(equalTo: workoutView.leadingAnchor, constant: 10).isActive = true
-                workoutName.trailingAnchor.constraint(equalTo: workoutView.trailingAnchor, constant: -10).isActive = true
-                
-                workoutView.addSubview(workoutDetails)
-                workoutDetails.translatesAutoresizingMaskIntoConstraints = false
-                workoutDetails.topAnchor.constraint(equalTo: workoutName.bottomAnchor, constant: 5).isActive = true
-                workoutDetails.leadingAnchor.constraint(equalTo: workoutView.leadingAnchor, constant: 10).isActive = true
-                workoutDetails.trailingAnchor.constraint(equalTo: workoutView.trailingAnchor, constant: -10).isActive = true
-                
-                workoutView.addSubview(workoutDescription)
-                workoutDescription.translatesAutoresizingMaskIntoConstraints = false
-                workoutDescription.topAnchor.constraint(equalTo: workoutDetails.bottomAnchor, constant: 5).isActive = true
-                workoutDescription.leadingAnchor.constraint(equalTo: workoutView.leadingAnchor, constant: 10).isActive = true
-                workoutDescription.trailingAnchor.constraint(equalTo: workoutView.trailingAnchor, constant: -10).isActive = true
-                workoutDescription.bottomAnchor.constraint(equalTo: workoutView.bottomAnchor, constant: -10).isActive = true
-                
-                self?.workoutStackView.addArrangedSubview(workoutView)
-                workoutView.translatesAutoresizingMaskIntoConstraints = false
-                workoutView.leadingAnchor.constraint(equalTo: self!.workoutStackView.leadingAnchor, constant: 10).isActive = true
-                workoutView.trailingAnchor.constraint(equalTo: self!.workoutStackView.trailingAnchor, constant: -10).isActive = true
-            }
+            self!.workoutsArr = documents
+            self?.aTableView.reloadData()
         }
     }
+    
+}
+extension ExploreWorkoutViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workoutsArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExploreCell", for: indexPath) as! ExploreCell
+        
+        let workout = self.workoutsArr[indexPath.row].data()
+        cell.nameLbl.text = workout!["name"] as? String ?? ""
+        
+        let workoutType = workout!["type"] as? String ?? ""
+        let skillLevel = workout!["skill_level"] as? String ?? ""
+        let muscleGroup = workout!["muscle_group"] as? String ?? ""
+        let goal = workout!["goal"] as? String ?? ""
+        
+        
+        //
+        cell.mergeLbl.text = "\(muscleGroup) | \(workoutType) | \(skillLevel) | \(goal)"
+        cell.descLbl.text = workout!["workout_description"] as? String ?? ""
+        
+        return cell
+        
+    }
+}
+
+class ExploreCell: UITableViewCell {
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mergeLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
     
 }
--- WiCSE Shadowing Project/ViewControllers/uploadWorkoutViewController.swift
+++ WiCSE Shadowing Project/ViewControllers/uploadWorkoutViewController.swift
@@ -23,76 +23,44 @@ class uploadWorkoutViewController: UIViewController {
     @IBOutlet weak var submitBtn: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
     
-    
-    @IBOutlet var skillCollection: [UIButton]!
-    @IBOutlet var goalCollection: [UIButton]! //not all goal buttons are connected
-    @IBOutlet var muscleCollection: [UIButton]! // not all muscle buttons are connected
     @IBOutlet var typeCollection: [UIButton]!
     
     let db = Firestore.firestore()
     let user = Auth.auth().currentUser
-    
+    var skillDropDown = DropDown()
+    var workoutTypeDropDown = DropDown()
+    let muscleGroupDropDown = DropDown()
+    let userGoalDropDown = DropDown()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setUpElements()
-        // drop down animation
-        skillCollection.forEach{ (btn) in
-            btn.isHidden = true
-            btn.alpha = 0
-        }
-        goalCollection.forEach{ (btn) in
-            btn.isHidden = true
-            btn.alpha = 0
-        }
-        muscleCollection.forEach{ (btn) in
-            btn.isHidden = true
-            btn.alpha = 0
-        }
-        typeCollection.forEach{ (btn) in
-            btn.isHidden = true
-            btn.alpha = 0
-        }
     }
     
     @IBAction func skillPressed(_ sender: UIButton) {
-        skillCollection.forEach{ (btn) in
-            UIView.animate(withDuration: 0.7){
-                btn.isHidden = !btn.isHidden
-                btn.alpha = btn.alpha == 0 ? 1 : 0
-                btn.layoutIfNeeded()
-            }
-        }
+        
+        skillDropDown.show()
+        
     }
-    
-    @IBAction func selectedSkill(_ sender: UIButton) {
-        if let btnLabel = sender.titleLabel?.text{
-            print(btnLabel)
-        }
-    }
-    
     @IBAction func goalPressed(_ sender: UIButton) {
-        goalCollection.forEach{ (btn) in
-            UIView.animate(withDuration: 0.7){
-                btn.isHidden = !btn.isHidden
-                btn.alpha = btn.alpha == 0 ? 1 : 0
-                btn.layoutIfNeeded()
-            }
-        }
+        userGoalDropDown.show()
     }
     
     
     @IBAction func musclePressed(_ sender: UIButton) {
-        muscleCollection.forEach{ (btn) in
-            UIView.animate(withDuration: 0.7){
-                btn.isHidden = !btn.isHidden
-                btn.alpha = btn.alpha == 0 ? 1 : 0
-                btn.layoutIfNeeded()
-            }
+        muscleGroupDropDown.show()
+    }
+    
+    @IBAction func typePressed(_ sender: UIButton) {
+        workoutTypeDropDown.show()
+    }
+    
+    @IBAction func selectedSkill(_ sender: UIButton) {
+        if let btnLabel = sender.titleLabel?.text{
+            print(skillDropDown.selectedItem!)
         }
     }
-
     @IBAction func selectedGoal(_ sender: UIButton) {
         if let btnLabel = sender.titleLabel?.text{ //wrong connection switch w one belwo
             print(btnLabel)
@@ -105,15 +73,7 @@ class uploadWorkoutViewController: UIViewController {
         }
     }
     
-    @IBAction func typePressed(_ sender: UIButton) {
-        typeCollection.forEach{ (btn) in
-            UIView.animate(withDuration: 0.7){
-                btn.isHidden = !btn.isHidden
-                btn.alpha = btn.alpha == 0 ? 1 : 0
-                btn.layoutIfNeeded()
-            }
-        }
-    }
+    
     
     @IBAction func selectedType(_ sender: UIButton) {
         if let btnLabel = sender.titleLabel?.text{
@@ -148,7 +108,7 @@ class uploadWorkoutViewController: UIViewController {
     
     @IBAction func submitClicked(_ sender: Any) {
         let error = validateFields()
-       // let ref = Database.database().reference()
+        // let ref = Database.database().reference()
         let user = Auth.auth().currentUser
         if error != nil{
             showError("Oops! Looks like you forgot something :/")
@@ -188,7 +148,7 @@ class uploadWorkoutViewController: UIViewController {
                 "uid": uid
             ]) { error in
                 if let error = error {
-                   showError("Error saving workout: \(error.localizedDescription)")
+                    showError("Error saving workout: \(error.localizedDescription)")
                 } else {
                     transitionToExplore()
                 }
@@ -209,32 +169,43 @@ class uploadWorkoutViewController: UIViewController {
         // hide the error label
         errorLabel.alpha = 0
         
-        let userGoalDropDown = DropDown()
+        skillDropDown.anchorView = userSkill
+        skillDropDown.bottomOffset = CGPoint(x: 0, y:(skillDropDown.anchorView?.plainView.bounds.height)! + 5)
+        skillDropDown.dataSource = ["beginner", "intermediate", "expert"]
+        skillDropDown.width = 105
+        DropDown.appearance().setupCornerRadius(8) // available since v2.3.6
+        
+        skillDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            self.userSkill.setTitle(item, for: .normal)
+        }
+        
+        
+        
         userGoalDropDown.anchorView = userGoal
+        userGoalDropDown.bottomOffset = CGPoint(x: 0, y:(userGoalDropDown.anchorView?.plainView.bounds.height)! + 5)
+        
         userGoalDropDown.dataSource = ["build muscle", "tone", "strength train"]
         userGoalDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
             self.userGoal.setTitle(item, for: .normal)
         }
         
-        let muscleGroupDropDown = DropDown()
         muscleGroupDropDown.anchorView = userMuscleGroup
+        muscleGroupDropDown.bottomOffset = CGPoint(x: 0, y:(muscleGroupDropDown.anchorView?.plainView.bounds.height)! + 5)
+        
         muscleGroupDropDown.dataSource = ["chest", "back", "legs", "shoulders", "arms", "abs", "glutes", "calves"]
+        muscleGroupDropDown.width = 90
         muscleGroupDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
             self.userMuscleGroup.setTitle(item, for: .normal)
         }
         
-        let skillDropDown = DropDown()
-        skillDropDown.anchorView = userSkill
-        skillDropDown.dataSource = ["beginner", "intermediate", "expert"]
-        skillDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            self.userSkill.setTitle(item, for: .normal)
-        }
+    
+        workoutTypeDropDown.anchorView = userSkill
+        workoutTypeDropDown.bottomOffset = CGPoint(x: userWorkoutType.frame.origin.x, y:(workoutTypeDropDown.anchorView?.plainView.bounds.height)! + 5)
         
-        let workoutTypeDropDown = DropDown()
-        workoutTypeDropDown.anchorView = userWorkoutType
         workoutTypeDropDown.dataSource = ["plyometrics", "power lifting", "strength", "stretch"]
+        workoutTypeDropDown.width = 105
         workoutTypeDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            self.userSkill.setTitle(item, for: .normal)
+            self.userWorkoutType.setTitle(item, for: .normal)
         }
         
     }
